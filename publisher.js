ac0a1f4381b4b83756b1ab6a288ed7846439ed5f
Mert POLAT <90008958+mert2m@users.noreply.github.com>
2023-06-14 18:09:20 +0300

    Update

--- publisher.js
+++ publisher.js
@@ -1,10 +1,14 @@
 const redis = require("redis");
+
 const client = redis.createClient();
 
-client.on("error", error => {
-    console.error(error);
+client.on("error", (error) => {
+  console.error(error);
 });
 
-client.publish("mertpolat", "app üzerinden gönderilen", (e, number) => {
-    console.log(`Mesaj ${number} kişiye gonderildi`);
-})
+client.publish("mertpolat", "app üzerinden gönderilen", (error, number) => {
+  if (error) {
+    console.error(error);
+  }
+  console.log(`Mesaj ${number} kişiye gönderildi`);
+});
